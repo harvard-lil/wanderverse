821b98e5128c06012b0bc60f5184984d909a5c9a
Anastasia Aizman <anastasia.aizman@gmail.com>
2020-03-20 18:37:00 -0400

    add - first models, static assets, requirements

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    author = models.CharField(max_length=1000, null=True, blank=True)
+    title = models.TextField(null=True, blank=True)
+
+class Poem(models.Model):
+    first_line = models.TextField(blank=True, null=True)
+    first_book = models.ForeignKey(Book, related_name='first_line', on_delete=models.DO_NOTHING)
+    second_line = models.TextField(blank=True, null=True)
+    second_book = models.ForeignKey(Book, related_name='second_line', on_delete=models.DO_NOTHING)
--- config/settings/settings_base.py
+++ config/settings/settings_base.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
+
     'app',
 ]
 
@@ -75,9 +77,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wanderverse',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
 }
 
 
@@ -118,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "../static"),
+]
